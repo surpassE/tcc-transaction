39186d0b0c109ac115ae8bf5c6f0e964e2a8a0bf
dingzc <zhichaoding@hongkun.com.cn>
2017-08-21 16:23:04 +0800

    sample-添加测试启动脚本&更新数据库测试地址

---
+++ tcc-transaction-tutorial-sample/tcc-transaction-dubbo-sample/tcc-transaction-dubbo-redpacket/src/main/java/org/mengyun/tcctransaction/sample/dubbo/redpacket/StartRed.java
@@ -0,0 +1,22 @@
+package org.mengyun.tcctransaction.sample.dubbo.redpacket;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class StartRed {
+
+	public static void main(String[] args) throws Exception {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+				"classpath:config/spring/local/appcontext-service-dao.xml",
+				"classpath:config/spring/local/appcontext-service-datasource.xml",
+				"classpath:config/spring/local/appcontext-service-provider.xml",
+				"classpath:config/spring/local/appcontext-web-servlet.xml",
+				"classpath:tcc-transaction-dubbo.xml",
+				"classpath:tcc-transaction.xml"
+				);
+		context.start();
+		synchronized (Thread.currentThread()) {
+			Thread.currentThread().wait();
+		}
+		context.close();
+	}
+}
